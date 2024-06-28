f1893497ecf81121b8ea3dbabd86fd5f2d974413
Vinay Kumar <vinaykumarpatha1701@gmail.com>
2024-06-28 12:54:49 +0530

    imported prism library

---
+++ src/prism-import.ts
@@ -0,0 +1,8 @@
+// src/prism-imports.ts
+import Prism from "prismjs";
+import "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css"; // or another theme of your choice
+
+export default Prism;
